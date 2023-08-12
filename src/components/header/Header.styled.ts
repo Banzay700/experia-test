6859e286bb1c7b43c0829591d818b8438e642c39
Oleksandr Lotko <lotkoolexandr@gmail.com>
2023-08-12 12:28:03 +0300

    add style

--- src/components/header/Header.styled.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export const Wrapper = styled.div`
-  display: flex;
-  padding: 16px 48px 16px 32px;
-  background-color: ${({ theme }) => theme.background.secondary};
-  align-items: center;
-  justify-content: space-between;
-`
